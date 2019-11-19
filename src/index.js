8f18ede338157e20c5e3c2773285548639269f60
David Hartsough <hartsoughdavid@gmail.com>
2019-11-19 02:55:04 -0700

    prepare for first deploy

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from "./serviceWorker";
 import initFirebase from "./fb";
 
 initFirebase();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// serviceWorker.unregister();
+// serviceWorker.register();
